0c5313c7d32a3e9c48f032889599f14a7ec5e36f
Daniel Bluhm <bluhmdan@gmail.com>
2018-05-15 14:28:15 -0600

    Fixed several errors; can now accept conn_reqs
    Signed-off-by: Daniel Bluhm <bluhmdan@gmail.com>

--- python/modules/connection.py
+++ python/modules/connection.py
@@ -1,36 +1,35 @@
-class Connection:
+'''
+    Handles connection requests from other peers.
+'''
+def handle_request(data, wallet_handle):
+    print("Connection request received")
+    print(data)
+    print(wallet_handle)
 
-    '''
-        Handles connection requests from other peers.
-    '''
-    def handle_request(self, data, wallet_handle):
+'''
+    decrypts anoncrypted connection response
+'''
+def handle_response(data, wallet_handle):
 
-        pass
+    pass
 
-    '''
-        decrypts anoncrypted connection response 
-    '''
-    def handle_response(self, data, wallet_handle):
+'''
+    sends a connection request.
 
-        pass
+    a connection response contains the user's did, verkey, endpoint, and endpoint of person wanting to connect.
+'''
+def send_request(data, wallet_handle):
 
-    '''
-        sends a connection request. 
-        
-        a connection response contains the user's did, verkey, endpoint, and endpoint of person wanting to connect.
-    '''
-    def send_request(self, data, wallet_handle):
+    pass
 
-        pass
+'''
+    sends a connection response should be anon_encrypted.
 
-    '''
-        sends a connection response should be anon_encrypted. 
-        
-        a connection response will include: 
-        
-        - user DID, and verkey
-        
-    '''
-    def send_response(self, data, wallet_handle):
+    a connection response will include:
 
-        pass
+    - user DID, and verkey
+
+'''
+def send_response(data, wallet_handle):
+
+    pass
