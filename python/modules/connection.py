2963f45b3925b01cd08d39946543236f498402b6
Michael Boyd <michaeldavisboyd@gmail.com>
2018-05-15 15:46:11 -0600

    removed merge conflicts
    Signed-off-by: Michael Boyd <michaeldavisboyd@gmail.com>

--- python/modules/connection.py
+++ python/modules/connection.py
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 import asyncio
 import time
 import re
 from indy import crypto, did, wallet
 
-
-=======
->>>>>>> 06e09cf26898c92ae1cc5599e171089cdc360d21
 '''
     Handles connection requests from other peers.
 '''
 def handle_request(data, wallet_handle):
-<<<<<<< HEAD
 
     pass
 
@@ -23,7 +18,6 @@ def handle_response(self, data, wallet_handle):
     decrypted = await crypto.auth_decrypt(wallet_handle, my_vk, data)
     msg = decrypted.__getitem__(1).decode()
     print(msg)
-=======
     print("Connection request received")
     print(data)
     print(wallet_handle)
@@ -32,18 +26,12 @@ def handle_response(self, data, wallet_handle):
     decrypts anoncrypted connection response
 '''
 def handle_response(data, wallet_handle):
->>>>>>> 06e09cf26898c92ae1cc5599e171089cdc360d21
 
     pass
 
 '''
-<<<<<<< HEAD
     sends a connection request. 
-    
-=======
-    sends a connection request.
 
->>>>>>> 06e09cf26898c92ae1cc5599e171089cdc360d21
     a connection response contains the user's did, verkey, endpoint, and endpoint of person wanting to connect.
 '''
 def send_request(data, wallet_handle):
@@ -51,21 +39,11 @@ def send_request(data, wallet_handle):
     pass
 
 '''
-<<<<<<< HEAD
     sends a connection response should be anon_encrypted. 
     
     a connection response will include: 
     
     - user DID, and verkey
-    
-=======
-    sends a connection response should be anon_encrypted.
-
-    a connection response will include:
-
-    - user DID, and verkey
-
->>>>>>> 06e09cf26898c92ae1cc5599e171089cdc360d21
 '''
 def send_response(data, wallet_handle):
 
