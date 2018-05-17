bf6457b550ec09135d038c3aebbca0ee6c9a87b6
Daniel Bluhm <bluhmdan@gmail.com>
2018-05-17 16:37:45 -0600

    Receive requests into pending list
    Signed-off-by: Daniel Bluhm <bluhmdan@gmail.com>

--- python/indy-agent.py
+++ python/indy-agent.py
@@ -2,14 +2,16 @@ import asyncio
 import os
 import sys
 from aiohttp import web
+from aiohttp_index import IndexMiddleware
 from indy import did, wallet, pool
 
-import view
-from receiver.aiohttp_receiver import AioHttpReceiver as Receiver
+from receiver.message_receiver import MessageReceiver as Receiver
 from router.simple_router import SimpleRouter as Router
+
+from model.agent import Agent
 import modules.connection as connection
+import modules.init as init
 import serializer.json_serializer as Serializer
-from aiohttp_index import IndexMiddleware
 
 if 'INDY_AGENT_PORT' in os.environ.keys():
     port = int(os.environ['INDY_AGENT_PORT'])
@@ -20,12 +22,14 @@ loop = asyncio.get_event_loop()
 agent = web.Application(middlewares=[IndexMiddleware()])
 agent['msg_router'] = Router()
 agent['msg_receiver'] =  Receiver(asyncio.Queue())
-agent['initialized'] = False
+agent['agent'] = Agent()
 
 routes = [
     web.static('/', os.path.realpath('view/')),
     web.post('/indy', agent['msg_receiver'].handle_message),
-    web.post('/indy/init', view.init)
+    web.post('/indy/init', init.initialize_agent),
+    web.get('/indy/connections', connection.connections),
+    web.get('/indy/requests', connection.requests),
 ]
 
 agent.add_routes(routes)
@@ -35,21 +39,21 @@ loop.run_until_complete(runner.setup())
 
 server = web.TCPSite(runner, 'localhost', port)
 
-async def main(agent):
+async def message_process(agent):
     msg_router = agent['msg_router']
     msg_receiver = agent['msg_receiver']
 
-    await msg_router.register("CONN_REQ", connection.handle_request)
+    await msg_router.register("CONN_REQ", connection.handle_request_received)
     await msg_router.register("CONN_RES", connection.handle_response)
 
     while True:
         msg_bytes = await msg_receiver.recv()
         msg = Serializer.unpack(msg_bytes)
-        await msg_router.route(msg, agent['wallet_handle'])
+        await msg_router.route(msg, agent['agent'])
 
 try:
     loop.create_task(server.start())
-    loop.create_task(main(agent))
+    loop.create_task(message_process(agent))
     loop.run_forever()
 except KeyboardInterrupt:
     print("exiting")
---
+++ python/view/__init__.py
@@ -0,0 +1,6 @@
+import aiohttp
+from aiohttp import web
+
+def require_init(agent):
+    if not agent.initialized
+        raise web.HTTPFound('/init')
