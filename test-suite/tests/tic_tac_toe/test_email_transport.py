dd5a79094c00e4058bc0890ceb09032c4ca35368
ike <chondool@gmail.com>
2019-02-08 12:01:37 -0700

    added ping-pong protocol test with email_transport
    Signed-off-by: ike <chondool@gmail.com>

--- test-suite/tests/tic_tac_toe/test_email_transport.py
+++ test-suite/tests/tic_tac_toe/test_email_transport.py
@@ -22,3 +22,27 @@ async def test_got_ttt_res(emailTransport):
 
     received_msg = json.loads(received_msg.decode("utf-8"))
     assert received_msg["@type"] == 'did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/tictactoe/1.0/move'
+
+@pytest.mark.asyncio
+async def test_ping_pong(emailTransport):
+    msg_send = Message({
+      "@type": "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/trust_ping/1.0/ping",
+      "@id": "518be002-de8e-456e-b3d5-8fe472477a86",
+      "@timing": {
+        "out_time": "2018-12-15 04:29:23Z",
+        "expires_time": "2018-12-15 05:29:23Z",
+        "delay_milli": 0
+      },
+      "comment_ltxt": "Hi. Are you listening?",
+      "response_requested": True
+    })
+
+    securemsg = emailTransport.securemsg
+
+    msg = await pack(securemsg.wallet_handle, securemsg.my_vk, securemsg.their_vk, msg_send)
+
+    received_msg = emailTransport.send_encrypted_store_resp(msg)
+
+    received_msg = json.loads(received_msg.decode("utf-8"))
+    assert received_msg["@type"] == "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/trust_ping/1.0/ping"
+
